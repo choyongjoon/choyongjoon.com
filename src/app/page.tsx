d1cade29336decf4e909c45ddcd15d9c11fcca2c
Yongjun Cho <yongjun.cho@bearrobotics.ai>
2025-07-25 09:54:18 +0900

    fix links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,12 @@ export default function Home() {
 				<p>Personal website exploring ideas</p>
 				<ol>
 					<li className="mb-2 tracking-[-.01em]">
-						<a className="link" href="https://old-blog.choyongjoon.com">
+						<a
+							className="link"
+							href="https://old-blog.choyongjoon.com"
+							target="_blank"
+							rel="noopener"
+						>
 							Old blog
 						</a>
 					</li>
@@ -14,17 +19,32 @@ export default function Home() {
 				<div className="divider"></div>
 				<ol>
 					<li>
-						<a className="link" href="https://bsky.app/profile/choyongjoon.com">
+						<a
+							className="link"
+							href="https://bsky.app/profile/choyongjoon.com"
+							target="_blank"
+							rel="noopener"
+						>
 							Bluesky
 						</a>
 					</li>
 					<li>
-						<a className="link" href="https://github.com/choyongjoon">
+						<a
+							className="link"
+							href="https://github.com/choyongjoon"
+							target="_blank"
+							rel="noopener"
+						>
 							GitHub
 						</a>
 					</li>
 					<li>
-						<a className="link" href="https://www.linkedin.com/in/choyongjoon/">
+						<a
+							className="link"
+							href="https://www.linkedin.com/in/choyongjoon/"
+							target="_blank"
+							rel="noopener"
+						>
 							LinkedIn
 						</a>
 					</li>
